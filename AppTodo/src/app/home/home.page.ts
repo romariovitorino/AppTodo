136046926b05254fb515aebc40676f5605a0a235
Romário Vitorino <romariovitorino.tech@gmail.com>
2022-04-28 13:30:04 -0400

    Implementa texto para digitar tarefas

---
+++ AppTodo/src/app/home/home.page.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private alertCrtl: AlertController) {}
+
+  async showAdd() {
+    const alert = await this.alertCrtl.create({
+      cssClass: 'my-custom-class',
+      header: 'O que você deseja fazer?',
+      inputs: [
+        {
+          name: 'tarefa1',
+          type: 'text',
+          placeholder: 'Digite o que deseja fazer.',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Cancelado com sucesso!');
+          },
+        },
+        {
+          text: 'Adicionar',
+          handler: () => {
+            console.log('Adicionado com sucesso!');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+}
+
+}
+
