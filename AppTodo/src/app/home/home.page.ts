07a79a844b219a2111ae09cd246bf4b51e069f7d
Romário Vitorino <romariovitorino.tech@gmail.com>
2022-05-11 17:14:18 -0400

    customizando pagina home

--- AppTodo/src/app/home/home.page.ts
+++ AppTodo/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { ActionSheetController, AlertController, ToastController } from '@ionic/angular';
+import { toastController } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,16 @@ import { AlertController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private alertCrtl: AlertController) {}
+  // adicionando uma lista de tarefas( um array de objetos para a tarefa)
+  tarefas: any[] = [];
+  constructor(private alertCrtl: AlertController, private toastCtrl: ToastController, private actionSheetCrtl: ActionSheetController) {
+    let tarefaSalva = localStorage.getItem('tarefaUsuario');
+
+    if (tarefaSalva != null) {
+      this.tarefas = JSON.parse(tarefaSalva);
+    }
+
+  }
 
   async showAdd() {
     const alert = await this.alertCrtl.create({
@@ -32,15 +42,81 @@ export class HomePage {
         },
         {
           text: 'Adicionar',
-          handler: () => {
-            console.log('Adicionado com sucesso!');
+          handler: (form) => {
+            this.adicionaTarefa(form.tarefa1);
           },
         },
       ],
     });
 
     await alert.present();
-}
+  }
+
+  // método para adicionar uma tarefa no local storage
+  async adicionaTarefa(novaTarefa: string) {
+    // verifica se a variável esta vazia ou não
+    if (novaTarefa.trim().length < 1) {
+      // caso a variável esteja vazia, irá apresentar uma mensagem no topo da tela
+      const toast = await this.toastCtrl.create({
+        // mensagem que será apresentada
+        message: 'Por favor, digite a tarefa!',
+        // duração da mensagem( 2 segundos neste exemplo)
+        duration: 2000,
+        // posição da mensagem, neste caso será no topo.
+        position: 'top',
+      });
+
+      toast.present();
+      return;
+    }
+
+    // definição de uma variável com a estrutura da nossa tarefa
+    const tarefa = { nome: novaTarefa, realizada: false };
+    // adicionado a tarefa em uma lista de tarefas
+    this.tarefas.push(tarefa);
+
+    //depois de atualizar a nossa lista de tarefa, chama a função para salvar no Local Storage
+    this.salvaLocalStorage();
+  }
+
+  salvaLocalStorage() {
+    localStorage.setItem('tarefaUsuario', JSON.stringify(this.tarefas));
+  }
+
+
+  async realizaAcoes(tarefa: any) {
+    const actionSheet = await this.actionSheetCrtl.create({
+      header: 'Qual ação realizar?',
+      buttons: [{
+        text: tarefa.realizada ? 'Desmarcar' : 'Marcar',
+        icon: tarefa.realizada ? 'checkmark-circle' : 'radio-button-off-outline',
+        handler: () => {
+          tarefa.realizada = !tarefa.realizada;
+          this.salvaLocalStorage();
+        }
+      },  {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role, data } = await actionSheet.onDidDismiss();
+
+  }
+
+   excluirTarefa(tarefa: any){
+     this.tarefas = this.tarefas.filter(arrayTarefa => tarefa != arrayTarefa);
+
+     this.salvaLocalStorage();
+
+  }
 
 }
+
+
 
